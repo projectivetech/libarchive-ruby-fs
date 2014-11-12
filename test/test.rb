467b274a40a6d04f51d886a230a41a550c2722af
Malte Rohde <malte.rohde@flavoursys.com>
2014-11-12 15:24:40 +0100

    Add failing test case for UTF8 symbols with combining diacritics.

--- test/test.rb
+++ test/test.rb
@@ -43,42 +43,51 @@ class TarTest < MiniTest::Test
 end
 
 class UnicodeTest < MiniTest::Test
-  def test_unicode_zip
-    file = "٩(-̮̮̃-̃)۶٩(●̮̮̃•̃)۶٩(͡๏̯͡๏)۶٩(-̮̮̃•̃)."
-    
-    LibArchiveRubyFs.write_open_filename('unicode.zip', LibArchiveRubyFs::COMPRESSION_COMPRESS, LibArchiveRubyFs::FORMAT_ZIP) do |ball|
-      ball.new_entry do |entry|
-        entry.copy_stat(__FILE__)
-        entry.pathname = file
-        ball.write_header(entry)
+  {
+    # Some unicode string in utf-8 within script.
+    "٩(-̮̮̃-̃)۶٩(●̮̮̃•̃)۶٩(͡๏̯͡๏)۶٩(-̮̮̃•̃)äöü." => 'in_script',
+    # 'xäx' in UTF-8.
+    [120, 195, 164, 120].pack('C*').force_encoding('UTF-8') => 'simple',
+    # 'xäx' in UTF-8 with a" combining characters.
+    [120, 97, 204, 136, 120].pack('C*').force_encoding('UTF-8') => 'combining'
+  }.each do |file, tname|
+    define_method("test_unicode_zip_#{tname}") do
+      begin
+        LibArchiveRubyFs.write_open_filename('unicode.zip', LibArchiveRubyFs::COMPRESSION_COMPRESS, LibArchiveRubyFs::FORMAT_ZIP) do |ball|
+          ball.new_entry do |entry|
+            entry.copy_stat(__FILE__)
+            entry.pathname = file
+            ball.write_header(entry)
+          end
+        end
+
+        LibArchiveRubyFs.read_open_filename('unicode.zip', LibArchiveRubyFs::COMPRESSION_COMPRESS, LibArchiveRubyFs::FORMAT_ZIP) do |ball|
+          entry = ball.next_header
+          assert_equal(entry.pathname, file)
+        end
+      ensure
+        FileUtils.rm_f('unicode.zip')
       end
     end
 
-    LibArchiveRubyFs.read_open_filename('unicode.zip', LibArchiveRubyFs::COMPRESSION_COMPRESS, LibArchiveRubyFs::FORMAT_ZIP) do |ball|
-      entry = ball.next_header
-      assert_equal(entry.pathname, file)
-    end
+    define_method("test_unicode_tar_#{tname}") do
+      begin
+        LibArchiveRubyFs.write_open_filename('unicode.tar', LibArchiveRubyFs::COMPRESSION_NONE, LibArchiveRubyFs::FORMAT_TAR) do |ball|
+          ball.new_entry do |entry|
+            entry.copy_stat(__FILE__)
+            entry.pathname = file
+            ball.write_header(entry)
+            ball.write_data(File.read(__FILE__))
+          end
+        end
 
-    FileUtils.rm('unicode.zip')
-  end
-
-  def test_unicode_tar
-    file = "٩(-̮̮̃-̃)۶٩(●̮̮̃•̃)۶٩(͡๏̯͡๏)۶٩(-̮̮̃•̃)."
-    
-    LibArchiveRubyFs.write_open_filename('unicode.tar', LibArchiveRubyFs::COMPRESSION_NONE, LibArchiveRubyFs::FORMAT_TAR) do |ball|
-      ball.new_entry do |entry|
-        entry.copy_stat(__FILE__)
-        entry.pathname = file
-        ball.write_header(entry)
-        ball.write_data(File.read(__FILE__))
+        LibArchiveRubyFs.read_open_filename('unicode.tar', LibArchiveRubyFs::COMPRESSION_NONE, LibArchiveRubyFs::FORMAT_TAR) do |ball|
+          entry = ball.next_header
+          assert_equal(entry.pathname, file)
+        end
+      ensure
+        FileUtils.rm('unicode.tar')
       end
     end
-
-    LibArchiveRubyFs.read_open_filename('unicode.tar', LibArchiveRubyFs::COMPRESSION_NONE, LibArchiveRubyFs::FORMAT_TAR) do |ball|
-      entry = ball.next_header
-      assert_equal(entry.pathname, file)
-    end
-
-    FileUtils.rm('unicode.tar')
   end
 end
